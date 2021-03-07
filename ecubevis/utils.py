36d43f44eba64c139a73a384ffc8092eb6baddbc
Carlos Gomez <carlos.gomez@bsc.es>
2021-03-07 13:06:26 +0100

    simplified slicing and longitude 0,360 or -180,180 handling

--- ecubevis/utils.py
+++ ecubevis/utils.py
@@ -1,9 +1,20 @@
 import numpy as np
 
-__all__ = ['slice_dataset']
+__all__ = ['slice_dataset',
+           'fix_longitude']
 
 
-def slice_dataset(ndarray, slice_time=None, slice_level=None, slice_lat=None, 
+def fix_longitude(data):
+    """ 
+    If the data central longitude is 180º (0º to 360º) then we set it to 0º 
+    (-180º to 180º)
+    """
+    data.coords['lon'] = (data.coords['lon'] + 180) % 360 - 180
+    data = data.sortby(data.lon)
+    return data
+
+
+def slice_dataset(data, slice_time=None, slice_level=None, slice_lat=None, 
                   slice_lon=None):
     """  
     Slice an N-dimensional ``xarray`` Dataset across its dimensions (time, level,
@@ -12,7 +23,7 @@ def slice_dataset(ndarray, slice_time=None, slice_level=None, slice_lat=None,
 
     Parameters
     ----------
-    ndarray : xarray Dataset
+    data : xarray Dataset
         Input N-dimensional dataset.
     slice_time : tuple of int or str or None, optional
         Tuple with initial and final values for slicing the time dimension. If 
@@ -29,38 +40,35 @@ def slice_dataset(ndarray, slice_time=None, slice_level=None, slice_lat=None,
 
     Returns
     -------
-    ndarray : xarray Dataset
+    data : xarray Dataset
         When any of the arguments ``slice_time``, ``slice_level``, 
         ``slice_lat``, ``slice_lon`` is defined (not None) the returned 
-        ``ndarray`` is the sliced input. If the slicing arguments are None the
+        ``data`` is the sliced input. If the slicing arguments are None the
         function returns the input Dataset.
 
     """ 
-    ndarray = check_coords(ndarray)
-    if slice_time is not None and 'time' in ndarray.coords:
+    data = check_coords(data)
+
+    # If central longitude is 180º (not 0º)
+    if np.any(data.lon > 180):
+        data = fix_longitude(data)
+
+    if slice_time is not None and 'time' in data.coords:
         if isinstance(slice_time, tuple) and isinstance(slice_time[0], str):
-            ndarray = ndarray.sel(time=slice(*slice_time))
+            data = data.sel(time=slice(*slice_time))
         elif isinstance(slice_time, tuple) and isinstance(slice_time[0], int):
-            ndarray = ndarray.isel(time=slice(*slice_time))
+            data = data.isel(time=slice(*slice_time))
     
-    if slice_level is not None and 'level' in ndarray.coords:
-        ndarray = ndarray.isel(level=slice(*slice_level))
-    
-    if slice_lat is not None and 'lat' in ndarray.coords:
-        ndarray = ndarray.sel(dict(lat=ndarray.lat[(ndarray.lat > slice_lat[0]) & 
-                                                   (ndarray.lat < slice_lat[1])]))
-    
-    if slice_lon is not None and 'lon' in ndarray.coords:
-        # -180 to 180
-        if ndarray.lon[0].values < 0:
-            ndarray = ndarray.sel(dict(lon=ndarray.lon[(ndarray.lon > slice_lon[0]) & 
-                                                       (ndarray.lon < slice_lon[1])]))
-        # 0 to 360
-        else:
-            ndarray = lon_wrap_slice(ndarray, wrap=360, indices=(slice_lon[0], 
-                                                                 slice_lon[1]))
-    
-    return ndarray
+    if slice_level is not None and 'level' in data.coords:
+        data = data.isel(level=slice(*slice_level))
+
+    if slice_lat is not None and 'lat' in data.coords:
+        data = data.sel(lat=slice(*slice_lat))
+
+    if slice_lon is not None and 'lon' in data.coords:
+        data = data.sel(lon=slice(*slice_lon))
+
+    return data
 
 
 def check_coords(ndarray):
@@ -78,51 +86,3 @@ def check_coords(ndarray):
             ndarray = ndarray.rename({alternative_names[i]: standard_names[i]})
     return ndarray
 
-
-def lon_wrap_slice(ds, wrap=360, dim='lon', indices=(-25,45)):
-    """
-    wrap selection assuming 360 degrees. xarray core is not likely to include 
-    this. Shame as would make nice method. Maybe someone builds something on 
-    xarray that supports geographical co-ordinates..
-    
-    Parameters
-    ----------
-    :param ds: dataset
-    :param wrap: default 360 -- period of co-ordinate.
-    
-    Notes
-    -----
-    https://github.com/pangeo-data/pangeo/issues/670
-    https://gis.stackexchange.com/questions/205871/xarray-slicing-across-the-antimeridian
-
-    # adjust the coordinate system of your data so it is centered over the anti-meridian instead
-    # https://gis.stackexchange.com/questions/205871/xarray-slicing-across-the-antimeridian
-    # t2m_83 = t2m_83.assign_coords(lon=(t2m_83.lon % 360)).roll(lon=(t2m_83.dims['lon'] // 2))
-
-    """    
-    sliceObj = slice(*indices)
-
-    l = (ds[dim] + 2 * wrap) % wrap
-    lstart = (sliceObj.start + 2 * wrap) % wrap
-    lstop = (sliceObj.stop + 2 * wrap) % wrap
-    if lstop > lstart:
-        ind = (l <= lstop) & (l >= lstart)
-    else:
-        ind = (l <= lstop) | (l >= lstart)
-
-    result = ds.isel(**{dim: ind})
-
-    # need to make data contigious which may require rolling it and adjusting 
-    # co-ord values.
-
-    # step 1 work out how much to roll co-ordinate system. Need to find "break"
-    # break is last place in array where ind goes from False to True
-    brk = np.max(np.where(ind.values[:-1] != ind.values[1:]))
-    brk = len(l) - brk - 1
-    result = result.roll(**{dim: brk})
-    # step 2 fix co-ords
-    v = result[dim].values
-    v = np.where(v < 180., v, v - 360)
-    result = result.assign_coords(**{dim: v})
-
-    return result
