d32e12200b79593b70b9e946b32e2c4acb329748
Carlos Gomez <carlos.gomez@bsc.es>
2021-12-11 10:42:47 +0100

    big improvements to load_transform_mfdataset, now works in parallel

--- ecubevis/io.py
+++ ecubevis/io.py
@@ -1,5 +1,6 @@
 from glob import glob
 import xarray as xr
+from joblib import Parallel, delayed
 
 
 import warnings
@@ -8,8 +9,8 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 __all__ = ['load_transform_mfdataset']
 
 
-def load_transform_mfdataset(dir_path, dim, transform_func=None, 
-                             transform_params={}):
+def load_transform_mfdataset(dir_path, dim='time', transform_func=None, 
+                             transform_params={}, n_jobs=1):
     """
     Read a multi-file distributed N-dimensional ``xarray`` dataset.
 
@@ -20,9 +21,12 @@ def load_transform_mfdataset(dir_path, dim, transform_func=None,
     dim : str
         Dimension to concatenate along.
     transform_func : function or None
-        Transform function to be applied to each file before loading it. 
+        Transform function to be applied to each xr.Dataset or xr.DataArray 
+        before loading and concatenating it. 
     transform_params : dict, optional
         Parameters of the transform function.
+    n_jobs : int, optional
+        Number of jobs.
 
     Returns
     -------
@@ -43,9 +47,12 @@ def load_transform_mfdataset(dir_path, dim, transform_func=None,
             ds.load()
             return ds
 
-    dir_path = dir_path + '/*.nc'
+    if not dir_path.endswith('*.nc'):
+        dir_path = dir_path + '/*.nc'
     paths = sorted(glob(dir_path))
-    datasets = [process_one_path(p) for p in paths]
+    print(f'Pre-processing {len(paths)} paths with `{transform_func.__name__}` function using {n_jobs} jobs')
+    datasets = Parallel(n_jobs=n_jobs)(delayed(process_one_path)(file_path) for file_path in paths)
+    print(f'Concatenating the preprocessed datasets along {dim}')
     combined = xr.concat(datasets, dim)
     return combined
 
